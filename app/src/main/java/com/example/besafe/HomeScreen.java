54ea376dfcc7cd6c4f1b33a324b2b20c316cb74c
Ishita <ishitajindal1999@gmail.com>
2021-11-12 11:57:18 +0530

    updated menu button in Homescreen

--- app/src/main/java/com/example/besafe/HomeScreen.java
+++ app/src/main/java/com/example/besafe/HomeScreen.java
@@ -2,13 +2,25 @@ package com.example.besafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
+    Button menuButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        menuButton=(Button) findViewById(R.id.menuButton);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent menu=new Intent(HomeScreen.this,Menu.class);
+                startActivity(menu);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/besafe/Menu.java
+++ app/src/main/java/com/example/besafe/Menu.java
@@ -1,14 +1,21 @@
 package com.example.besafe;
 
+import static android.view.View.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
 }
